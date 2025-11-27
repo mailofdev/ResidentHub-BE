97c19f4c6141ae05a883117a43261d75ece07470
sidberlin <sidhcode@gmail.com>
2025-11-27 14:06:17 +0530

    Swagger installed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // --------------------------------
+  // Swagger configuration
+  // --------------------------------
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation for my NestJS project')
+    .setVersion('1.0')
+    .addBearerAuth() // optional
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // --------------------------------
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
