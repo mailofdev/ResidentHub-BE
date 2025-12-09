0be7f0bc703de5862f676075342777e02baaaefd
mailofdev <mailofdeveloper@gmail.com>
2025-12-09 18:53:55 +0530

    removed all old code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';  // ✅ import AuthModule
 
 @Module({
-  imports: [
-    PrismaModule,
-    AuthModule,   // ✅ Add AuthModule here
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
